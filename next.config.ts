3cadc755c248738ffd769f7aafbaf5502a8e41e8
Farhan Muhamed Jaffar <farhanmoozhikkal@gmail.com>
2025-06-07 01:50:44 +0530

    Fixed basePath and production build

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    basePath: "/", 
     output: "export",
     images: {
-        unoptimized: true 
-    }, 
+        unoptimized: true,
+    },
 };
+
 export default nextConfig;
